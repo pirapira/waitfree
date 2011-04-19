e0ba2cc2c1464d2b5db84b83c1973b6cd1f9afb9
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2011-04-19 23:07:46 +0900

    now working!

--- possessive.hs
+++ possessive.hs
@@ -24,20 +24,19 @@ instance Thread t => Thread (SucT t) where
     atid (SucT x) = succ $ atid x
 
 -- how to hide this implementation?
--- K (writing_destination writing_action)
-newtype K t a = K (t, IO (MV.MVar a), IO a)
+-- K (writing_action)
+-- (destination should be a premise)
+newtype K t a = K (t, IO a)
 
 spawn :: Thread t => K t ()
-spawn = K (t, d, return ())
-    where d = MV.newEmptyMVar
+spawn = K (t, return ())
     
 class IOFunctor w where
   wmap :: (a -> IO b) -> w a -> w b
 
 instance Thread t => IOFunctor (K t) where
-  wmap f (K (_, _, x)) = K (t, e, y)
+  wmap f (K (_, x)) = K (t, y)
     where
-      e = MV.newEmptyMVar
       y = x >>= f
 
 (>=>)       :: Monad m => (a -> m b) -> (b -> m c) -> (a -> m c)
@@ -45,8 +44,9 @@ f >=> g     = \x -> f x >>= g
 
           
 -- IOComonad. to be moved 
-class IOFunctor w => IOComonad w where
-   extract :: w a -> IO a
+class IOFunctor w => MVComonad w where
+   -- extract sharedBox producer = (newProducer, receiver)
+   extract :: MV.MVar a -> w a -> (w a, IO a)
    duplicate :: w a -> w (w a)
    extend :: (w a -> IO b) -> w a -> w b
 
@@ -60,52 +60,49 @@ mute _ = return ()
 -- xxx add law for IOComonad
 
 -- | 'extend' with the arguments swapped. Dual to '>>=' for monads.
-(=>>) :: IOComonad w => w a -> (w a -> IO b) -> w b
+(=>>) :: MVComonad w => w a -> (w a -> IO b) -> w b
 (=>>) = flip extend
 
 -- | Injects a value into the comonad.
-(.>>) :: IOComonad w => w a -> IO b -> w b
+(.>>) :: MVComonad w => w a -> IO b -> w b
 w .>> b = extend (\_ -> b) w
 
-instance Thread t => IOComonad (K t) where
-    extract (K (_, d, _)) = do
-      putStrLn "waiting"
-      val <- (d >>= MV.readMVar)
-      putStrLn "got value"
-      return val
-    extend trans (K (s, d, f)) = K (t, d', f')
-        where
-          d' = MV.newEmptyMVar
-          f' = do
-            d_ <- d'
-            f_ <- f
-            MV.putMVar d_ f_
-            trans $ K (s, d, f)
+instance Thread t => MVComonad (K t) where
+    extract box (K (c, f)) = (newProducer, receiver)
+      where
+        newProducer = K (c, f')
+        f' = do
+          x <- f
+          MV.putMVar box x
+          return x
+        receiver = do 
+          putStrLn "waiting_for_remote_value"
+          val <- MV.readMVar box
+          putStrLn "got value"
+          return val
+    extend trans r@(K (_, f)) = K (t, g)
+      where
+        g = do
+          f
+          trans r
 
--- (=>>) :: IOComonad w => w a -> (w a -> IO b) -> IO (w b)
-
-    
 -- -- which remote to take?  Experiment!
 
 remote :: Thread t => (a -> IO b) -> K t a -> K t b
 remote = wmap
 
-
 ret :: Thread t => IO a -> K t a
-ret y = K (t, e, y)
-   where
-     e = MV.newEmptyMVar
-
+ret y = K (t, y)
 
 -- internal representation of a job
-newtype L a = L (AbstractThreadId, IO ())
+newtype L = L (AbstractThreadId, IO ())
 
-ktol :: Thread t => Show a => K t a -> L a
-ktol (K (th, d, f)) = L (atid th, job_action d f)
+ktol :: Thread t => K t () -> L
+ktol (K (th, f)) = L (atid th, f)
 
 infixr 5 //
 
-(//) :: Thread t => K t () -> [L ()] -> [L ()]
+(//) :: Thread t => K t () -> [L] -> [L]
 (//) hd tl = (ktol hd) : tl
 
 
@@ -116,15 +113,6 @@ infixr 5 //
 
 
 -- these are internal
-
--- action of sending the result to the shared box
--- this should not be visible to the user.
-job_action :: Show a => IO (MV.MVar a) -> IO a -> IO ()
-job_action d c = do
-  d' <- d
-  c' <- c
-  putStrLn $ show c'
-  MV.putMVar d' c'
 
 type JobChannel = [IO ()]
 -- Just x is a next job
@@ -143,10 +131,10 @@ type ThreadPool =
 type JobPool = 
     Map.Map AbstractThreadId JobChannel
 
-execute :: [L ()] -> IO ()
+execute :: [L] -> IO ()
 execute = spawnPool >=> waitThread
 
-spawnPool :: [L ()] -> IO ThreadPool
+spawnPool :: [L] -> IO ThreadPool
 spawnPool = run . constructJobPool
 
 run :: JobPool -> IO ThreadPool
@@ -159,7 +147,7 @@ threadSpawn aid ch p = do
     thid <- forkIO $ worker ch fin
     return $ Map.insert aid (thid, fin) p'
 
-constructJobPool :: [L ()] -> JobPool
+constructJobPool :: [L] -> JobPool
 constructJobPool [] = Map.empty
 constructJobPool (L (aid, action) : tl) =
   Map.insertWith (++) aid [action] rest
@@ -172,7 +160,6 @@ waitThread = Map.fold threadWait $ return ()
 threadWait :: (ThreadId, MV.MVar ()) -> IO () -> IO ()
 threadWait (thid, fin) w = do
     MV.readMVar fin
-    putStrLn "detect finish"
     killThread thid
     w
     
@@ -191,27 +178,38 @@ embarassingly_parallel = l // m // []
 --------------------------------------------------
 -- example ping_pong
 
-reader :: K ZeroT String
-reader = ret $ do
-           putStrLn $ "reader start"
-           hFlush stdout
-           str <- getLine
-           putStrLn $ "reader got: " ++ str
-           return str
-
-reader' :: K ZeroT ()
-reader' = extend mute reader
-         
-writer :: K (SucT ZeroT) ()
+writer :: K ZeroT String
 writer = ret $ do
            putStrLn $ "writer start"
-           str <- extract reader
-           putStrLn $ "writer got" ++ str
-           
-reader_writer = reader' // writer // []
+           hFlush stdout
+           str <- getLine
+           putStrLn $ "writer got: " ++ str
+           return str
+
+writer' :: MV.MVar String -> K ZeroT ()
+writer' box = extend mute $ new_writer box
+
+new_writer :: MV.MVar String -> K ZeroT String
+new_writer box = fst $ reader_writer box
+         
+reader_writer :: MV.MVar String -> (K ZeroT String, K (SucT ZeroT) ())
+reader_writer box = (w,  reader)
+  where
+    (w, str) = extract box writer
+    reader = K (t, readeraction)
+    readeraction = do
+      putStrLn "reader start"
+      str' <- str
+      putStrLn $ "reader got: " ++ str'
+
+rw :: MV.MVar String -> [L]           
+rw box = reader // writer' box // []
+    where reader = snd $ reader_writer box
 
 main :: IO ()
-main = execute $ reader_writer
+main = do
+    box <- MV.newEmptyMVar
+    execute $ rw box
     
 -- example waitfree
 
