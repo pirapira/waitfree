c72b23dcec0e7edebe482121badacd1ba89a14cc
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2011-04-21 07:15:53 +0900

    strange behaviour. requires too much inputs

--- possessive.hs
+++ possessive.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE TypeOperators, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, UndecidableInstances #-}
 
 import Control.Concurrent (ThreadId, forkIO, killThread)
-import qualified Control.Concurrent.MVar as MV
+import Control.Concurrent.MVar (MVar, tryPutMVar, putMVar, readMVar, newEmptyMVar, tryTakeMVar)
 import qualified Data.Map as Map
 import GHC.IO.Handle (hFlush)
 import System.IO (stdout)
@@ -66,13 +66,13 @@ type WithL a = ([L], a)
 -- first in [L] -> first in the queue
 -- the above effect -> should be first in the queue -> earlier in [L]
 
-writeMVar :: MV.MVar a -> Maybe a -> IO ()
+writeMVar :: MVar a -> Maybe a -> IO ()
 writeMVar box (Just v) = do
-    MV.tryPutMVar box v
+    tryPutMVar box v
     return ()
 writeMVar _ Nothing = return ()
                              
-comm :: Thread t => Thread s => HAppend l l' l'' => MV.MVar a -> MV.MVar c ->
+comm :: Thread t => Thread s => HAppend l l' l'' => MVar a -> MVar c ->
         WithL ((K t a) :*: l) -> WithL ((K s c) :*: l') -> WithL ((K t (a,c)) :*: (K s (c,a)) :*: l'')
 comm abox cbox (s0, HCons (K (taT, ta)) l) (s1, HCons (K (scT, sc)) l') =
     (news, K (taT, tatc) .*. K (scT, scsa) .*. hAppend l l')
@@ -91,19 +91,19 @@ comm abox cbox (s0, HCons (K (taT, ta)) l) (s1, HCons (K (scT, sc)) l') =
               (Nothing, _) -> return Nothing
               (_, Nothing) -> return Nothing
               (Just sc_, Just sa_) -> return $ Just (sc_, sa_)
-          tc = MV.tryTakeMVar cbox
-          sa = MV.tryTakeMVar abox
+          tc = tryTakeMVar cbox
+          sa = tryTakeMVar abox
           news = s0 ++ s1 ++ [ta_task] ++ [sc_task]
           ta_task = (atid taT, ta >>= writeMVar abox)     
           sc_task = (atid scT, sc >>= writeMVar cbox)     
 
-merge :: MV.MVar a -> WithL (IO (Maybe a) :*: IO (Maybe a) :*: l) -> WithL (IO (Maybe a) :*: l)
+merge :: MVar a -> WithL (IO (Maybe a) :*: IO (Maybe a) :*: l) -> WithL (IO (Maybe a) :*: l)
 merge box (s, (HCons x (HCons y l))) = (news, (HCons reader l))
   where
     news = s ++ [(-1, xwriter)] ++ [(-1, ywriter)]
     xwriter = x >>= writeMVar box
     ywriter = y >>= writeMVar box
-    reader = MV.tryTakeMVar box
+    reader = tryTakeMVar box
 
 data K t a = K (t, IO (Maybe a))
 
@@ -131,7 +131,7 @@ f >=> g     = \x -> f x >>= g
 -- IOComonad. to be moved 
 class IOMaybeFunctor w => MVComonad w where
    -- extract sharedBox producer = (newProducer, receiver)
-   extract :: MV.MVar a -> w a -> (w a, IO (Maybe a))
+   extract :: MVar a -> w a -> (w a, IO (Maybe a))
    duplicate :: w a -> w (w a)
    extend :: (w a -> IO (Maybe b)) -> w a -> w b
 
@@ -139,8 +139,11 @@ class IOMaybeFunctor w => MVComonad w where
        where g = wmap f
    duplicate = extend (return . Just)
 
-mute :: K t a -> IO ()
-mute _ = return ()
+mute :: Thread t => K t a -> L
+mute (K (t, a)) = (atid t, a >>= \_ -> return ()) 
+
+mute' :: IO (Maybe ()) -> L
+mute' e = (-1, e >>= \_ -> return ())
 
 -- xxx add law for IOComonad
 
@@ -162,7 +165,7 @@ instance Thread t => MVComonad (K t) where
           return x
         receiver = do 
           putStrLn "waiting_for_remote_value"
-          val <- MV.tryTakeMVar box
+          val <- tryTakeMVar box
           putStrLn "got value"
           return val
     extend trans r@(K (_, f)) = K (t, g)
@@ -201,8 +204,8 @@ rlineZero = ([], ret rline .*. HNil)
 rlineSucZero :: WithL ((K (SucT ZeroT) String) :*: HNil)
 rlineSucZero = ([], ret rline .*. HNil)
 
-commF :: MV.MVar String
-      -> MV.MVar String
+commF :: MVar String
+      -> MVar String
       -> WithL
          (K ZeroT (String, String)
                 :*: (K (SucT ZeroT) (String, String) :*: HNil))
@@ -227,22 +230,67 @@ eitherPrint (s, HCons e0 (HCons e1 l)) = (s, HCons e0' (HCons e1' l))
       e0' = extend printTwo e0
       e1' = extend printTwo e1
 
+step1 :: MVar String -> MVar String ->
+         WithL (K ZeroT () :*: (K (SucT ZeroT) () :*: HNil))
+step1 b0 b1 = eitherPrint $ commF b0 b1
+
+trivialize :: MVar () -> MVar () ->
+    WithL (K ZeroT () :*: (K (SucT ZeroT) () :*: HNil)) ->
+    WithL (IO (Maybe ()) :*: IO (Maybe ()) :*: HNil)
+trivialize box0 box1 (s, HCons e0 (HCons e1 l)) = (news, HCons e0' (HCons e1' l))
+    where
+      news = s ++ [mute k0] ++ [mute k1]
+      (k0, e0') = extract box0 e0
+      (k1, e1') = extract box1 e1
+
+
+step2 :: MVar String -> MVar String -> MVar () -> MVar () ->
+             WithL (IO (Maybe ()) :*: IO (Maybe ()) :*: HNil)
+step2 b0 b1 b2 b3 = trivialize b2 b3 $ step1 b0 b1
+
+merged :: MVar () ->
+          WithL (IO (Maybe ()) :*: IO (Maybe ()) :*: HNil) ->
+          WithL (IO (Maybe ()) :*: HNil)
+merged box conf = merge box conf
+
+final :: MVar String -> MVar String -> MVar () -> MVar () -> MVar () ->
+         WithL (IO (Maybe ()) :*: HNil)
+final b0 b1 b2 b3 b4 = merged b4 $ step2 b0 b1 b2 b3
+
+
 -- these are internal
 
 type JobChannel = [IO ()]
 
-worker :: JobChannel -> MV.MVar () -> IO ()
-worker [] fin = MV.putMVar fin ()
+worker :: JobChannel -> MVar () -> IO ()
+worker [] fin = putMVar fin ()
 worker (hd : tl) fin = do
   hd
   worker tl fin
 
 -- ThreadPool is finite map
 type ThreadPool =
-    Map.Map AbstractThreadId (ThreadId, MV.MVar ())
+    Map.Map AbstractThreadId (ThreadId, MVar ())
 
 type JobPool = 
     Map.Map AbstractThreadId JobChannel
+
+--- Produce [L]
+hypersequentToL :: WithL (IO (Maybe ()) :*: HNil) -> [L]
+hypersequentToL (s, HCons e HNil) = s ++ [lastjob]
+ where
+   lastjob = mute' e
+
+
+finalL :: MVar String -> MVar String -> MVar ()
+          -> MVar ()
+          -> MVar ()
+          -> [L]
+finalL b0 b1 b2 b3 b4 = hypersequentToL $ final b0 b1 b2 b3 b4 
+
+---
+--- What to do with [L]
+---
 
 execute :: [L] -> IO ()
 execute = spawnPool >=> waitThread
@@ -256,7 +304,7 @@ run = Map.foldrWithKey threadSpawn $ return Map.empty
 threadSpawn :: AbstractThreadId -> JobChannel -> IO ThreadPool -> IO ThreadPool
 threadSpawn aid ch p = do
     p' <- p
-    fin <- MV.newEmptyMVar
+    fin <- newEmptyMVar
     thid <- forkIO $ worker ch fin
     return $ Map.insert aid (thid, fin) p'
 
@@ -270,9 +318,9 @@ constructJobPool ((aid, action) : tl) =
 waitThread :: ThreadPool -> IO ()
 waitThread = Map.fold threadWait $ return ()
 
-threadWait :: (ThreadId, MV.MVar ()) -> IO () -> IO ()
+threadWait :: (ThreadId, MVar ()) -> IO () -> IO ()
 threadWait (thid, fin) w = do
-    MV.readMVar fin
+    readMVar fin
     killThread thid
     w
     
@@ -286,7 +334,13 @@ me :: K ZeroT ()
 me = spawn .>> (putStrLn "zero" >>= \_ -> return $ Just ())
 
 main :: IO ()
-main = undefined
+main = do
+  b0 <- newEmptyMVar
+  b1 <- newEmptyMVar
+  b2 <- newEmptyMVar
+  b3 <- newEmptyMVar
+  b4 <- newEmptyMVar
+  execute $ finalL b0 b1 b2 b3 b4
     
 -- example waitfree
 
